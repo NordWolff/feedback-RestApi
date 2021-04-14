6f7f4da93a43c56812faf279b71e41990b200b1c
Thomas <51476041+NordWolff@users.noreply.github.com>
2021-04-14 07:33:12 +0200

    FeedApplicationTests und JPA Entitys create

---
+++ src/main/java/de/dta/feed/feedback/repository/ThumbnailRepository.java
@@ -0,0 +1,7 @@
+package de.dta.feed.feedback.repository;
+
+import de.dta.feed.feedback.model.Thumbnail;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ThumbnailRepository extends CrudRepository<Thumbnail, Integer> {
+}
